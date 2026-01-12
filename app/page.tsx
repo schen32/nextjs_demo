8a19d4c925bf9921218aa22b900e6486edb1f99d
Simon Chen <simonc8192@gmail.com>
2026-01-12 08:02:45 -0800

    - fixed global style

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ const page = () => {
       <h1 className="text-center">
         The Hub for Every Dev <br></br> Even You Can't Must
       </h1>
-      <p>Hackatons, Meetups, and Conferences, All in One Place</p>
+      <p className="text-center mt-5">
+        Hackatons, Meetups, and Conferences, All in One Place
+      </p>
     </section>
   );
 };
