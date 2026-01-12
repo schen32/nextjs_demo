b681cac9024ed511b7ef74073f41a0eff5d0e540
Simon Chen <simonc8192@gmail.com>
2026-01-12 09:46:11 -0800

    - navBar component

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,15 @@ const page = () => {
         Hackatons, Meetups, and Conferences, All in One Place
       </p>
       <ExploreBtn></ExploreBtn>
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {[1, 2, 3, 4, 5].map((event) => (
+            <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
