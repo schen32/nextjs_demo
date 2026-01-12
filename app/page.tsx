427e40b955b871c32f8d51ef657e92c90250c102
Simon Chen <simonc8192@gmail.com>
2026-01-12 07:56:57 -0800

    - imported light ray component

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <p>Welcome to Next JS</p>
-    </div>
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br></br> Even You Can't Must
+      </h1>
+      <p>Hackatons, Meetups, and Conferences, All in One Place</p>
+    </section>
   );
 };
 
